35564427dc660a710bc7e7696f00f80f8e9d76b8
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-24 21:06:31 +0530

    insert null rows, closes #13

--- sqlscripts/table/insertRow.sql
+++ sqlscripts/table/insertRow.sql
@@ -1,2 +1,7 @@
 insert into users (username) values ("rambati");
 insert into users (id,username) values (1,"rambati");
+insert into users (id,username) values (2,null);
+insert into users (id) values (3);
+insert into users (username) values ("iambati");
+insert into users (id,username) values (null,"ravn");
+insert into users (id,username) values (null,null);
