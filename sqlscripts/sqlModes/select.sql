6fdd1ef55306e65befa018140a26dbc322b63c97
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-26 21:24:05 +0530

    select and set sql modes, closes #19

--- sqlscripts/sqlModes/select.sql
+++ sqlscripts/sqlModes/select.sql
@@ -1,3 +1,6 @@
 select @@GLOBAL.SQL_MODE;
 select @@SESSION.SQL_MODE;
 set sql_mode = 'STRICT_ALL_TABLES'
+select @@GLOBAL.sql_safe_updates;
+set sql_safe_updates=0; //not secure ex: delete from tableName; will be executed
+set sql_safe_updates=1; // secure ex: delete from tableName; will not be executed without any where condition
