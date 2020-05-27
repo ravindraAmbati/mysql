c35e97e40361fddb98129bdebf67a195b29fa6fb
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-27 20:23:13 +0530

    create a table with auto increment of primary key, closes #22

--- sqlscripts/table/insertRow.sql
+++ sqlscripts/table/insertRow.sql
@@ -9,3 +9,7 @@ insert into users (id,name,email) values (1,"ravindra","ravindra@example.com");
 insert into users (id,name,email) values (2,"ambati","ambati@example.com");
 insert into users (id) values (3);
 insert into users (id) values (4);
+insert into users (name) values ("ravindra");
+insert into users (name) values ("ambati");
+insert into users (id,name) values (3,"ambati");
+insert into users (id,name) values (0,"ambati"); // it does not accept 0 but auto increases the prior PK
