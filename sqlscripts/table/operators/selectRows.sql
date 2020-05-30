2d082e99cfa13e05513879c74f4ecff6214c0371
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-30 22:20:00 +0530

    select rows from users for >= and <= and between and NULL-safe and like and not, closes #25

--- sqlscripts/table/operators/selectRows.sql
+++ sqlscripts/table/operators/selectRows.sql
@@ -19,3 +19,15 @@ select * from users where age <=25 xor age <=30;
 select * from users where age <=30 xor age <=30;
 select * from users where age <=30 xor age <=30 xor age <=30;
 select * from users where age <=30 xor age <=30 xor age <=30 xor age<=30;
+/*Select all the rows where age is greater than 20 but less than 30*/
+select * from users where age > 20 and age >30;
+/*Select all rows where age is between 20 (inclusive) and 30 (inclusive)*/
+select * from users where age between 20 and 30;
+/*Select all rows where age is between 20 (inclusive) and 30 (inclusive) and the name is not “Vicky”*/
+select * from users where age between 20 and 30 and name <> "Vicky";
+/*Select all rows where either the name is null or the name does not contain the letter “e”*/
+select * from users where (name is null) or (name not like '%e%');
+/*Select all rows for users ages between 30 and 40 whether the name contains either “o” or “e”*/
+select * from users where (age between 30 and 40) or (name like '%e%' or '%o%');
+/*Select all rows where either the name contains an “o” or the id is less than 5, but not both*/
+select * from users where name like '%o%' xor id < 5;
